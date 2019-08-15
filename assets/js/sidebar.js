7da22a2574bfba89e1d9eae9855ae134889af82f
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-15 20:04:13 +0000

    Add about.html page

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -3,3 +3,18 @@ $("#sidebarCollapse").on("click", function() {
   $(".wrapper").toggleClass("menuup");
   $(".mobile-header").toggleClass("hide");
 });
+
+
+<nav>
+  <div class="inner-wrap">
+    <div class="site-header">Insert Name</div>
+    <div class="site-subheader">Web Developer</div>
+    <ul class="site-navigation">
+      <li>
+        <a href="index.html"><i class="fas fa-home"></i> Home</a>
+      </li>
+      <li>
+        <a href="about.html"><i class="fas fa-id-card"></i> About</a></li>
+    </ul>
+  </div>
+</nav>
